36483e08624baa52385a15ec7b6240577b80cf1f
ardns <rizkymardy@gmail.com>
2024-03-14 11:17:16 +0700

    add rate star

--- resources/views/pointakses/user/index.blade.php
+++ resources/views/pointakses/user/index.blade.php
@@ -64,6 +64,89 @@
             text-decoration: none;
             cursor: pointer;
         }
+        .rate {
+         float: left;
+         height: 46px;
+         padding: 0 10px;
+         }
+         .rate:not(:checked) > input {
+         position:absolute;
+         display: none;
+         }
+         .rate:not(:checked) > label {
+         float:right;
+         width:1em;
+         overflow:hidden;
+         white-space:nowrap;
+         cursor:pointer;
+         font-size:30px;
+         color:#ccc;
+         }
+         .rated:not(:checked) > label {
+         float:right;
+         width:1em;
+         overflow:hidden;
+         white-space:nowrap;
+         cursor:pointer;
+         font-size:30px;
+         color:#ccc;
+         }
+         .rate:not(:checked) > label:before {
+         content: '★ ';
+         }
+         .rate > input:checked ~ label {
+         color: #ffc700;
+         }
+         .rate:not(:checked) > label:hover,
+         .rate:not(:checked) > label:hover ~ label {
+         color: #deb217;
+         }
+         .rate > input:checked + label:hover,
+         .rate > input:checked + label:hover ~ label,
+         .rate > input:checked ~ label:hover,
+         .rate > input:checked ~ label:hover ~ label,
+         .rate > label:hover ~ input:checked ~ label {
+         color: #c59b08;
+         }
+         .star-rating-complete{
+            color: #c59b08;
+         }
+         .rated {
+         float: left;
+         height: 46px;
+         padding: 0 10px;
+         }
+         .rated:not(:checked) > input {
+         position:absolute;
+         display: none;
+         }
+         .rated:not(:checked) > label {
+         float:right;
+         width:1em;
+         overflow:hidden;
+         white-space:nowrap;
+         cursor:pointer;
+         font-size:30px;
+         color:#ffc700;
+         }
+         .rated:not(:checked) > label:before {
+         content: '★ ';
+         }
+         .rated > input:checked ~ label {
+         color: #ffc700;
+         }
+         .rated:not(:checked) > label:hover,
+         .rated:not(:checked) > label:hover ~ label {
+         color: #deb217;
+         }
+         .rated > input:checked + label:hover,
+         .rated > input:checked + label:hover ~ label,
+         .rated > input:checked ~ label:hover,
+         .rated > input:checked ~ label:hover ~ label,
+         .rated > label:hover ~ input:checked ~ label {
+         color: #c59b08;
+         }
+
     </style>
 
 
@@ -118,12 +201,25 @@
                                 </select>
                             </div>
                             <div class="form-group">
-                                <label for="rating">Rating:</label>
-                                <input type="number" name="rating" id="rating" class="form-control" min="1"
-                                    max="5" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="review">Review:</label>
+                                <div class="review">
+                                    <label for="review">Review:</label>
+                                </div>
+                                <div class="rate">
+                                    <input type="radio" id="star5" class="rate" name="rating" value="5"/>
+                                    <label for="star5" title="text">5 stars</label>
+                            
+                                    <input type="radio" checked id="star4" class="rate" name="rating" value="4"/>
+                                    <label for="star4" title="text">4 stars</label>
+                            
+                                    <input type="radio" id="star3" class="rate" name="rating" value="3"/>
+                                    <label for="star3" title="text">3 stars</label>
+                            
+                                    <input type="radio" id="star2" class="rate" name="rating" value="2">
+                                    <label for="star2" title="text">2 stars</label>
+                            
+                                    <input type="radio" id="star1" class="rate" name="rating" value="1"/>
+                                    <label for="star1" title="text">1 star</label>
+                                </div>
                                 <textarea name="review" id="review" class="form-control" rows="3"></textarea>
                             </div>
                             <button type="submit" class="btn btn-primary">Submit</button>
